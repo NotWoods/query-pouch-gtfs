0ac739a0586667d4ccc0211c1023d2036642f74b
Tiger Oakes <tigeroakes@gmail.com>
2017-06-05 18:38:00 -1000

    Removed extra code from source package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export * from './dump/';
-export { default, default as parseGTFS } from './dump/';
-export * from './load/';
 export * from './read/';
 export * from './interfaces';
 export * from './uri';
